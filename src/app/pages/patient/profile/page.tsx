1b6ec86389f6dd6bc200fb5deb1cd9664565f8b5
Biswaraj21 <111447635+Biswaraj21@users.noreply.github.com>
2024-05-08 09:12:20 +0530

    changes by biswa

--- src/app/pages/patient/profile/page.tsx
+++ src/app/pages/patient/profile/page.tsx
@@ -39,7 +39,7 @@ const App = () => {
     <div className="bg-slate-100 p-4">
       <h1 className="font-bold">Account & Details</h1>
       <div className="columns-2xs">
-        <div className="w-full aspect-square bg-slate-200 rounded-lg p-4 m-4 shadow-xl shadow-inner border-4 border-indigo-200 border-r-indigo-500">
+        <div className="w-full aspect-auto bg-slate-200 rounded-lg p-4 m-4 shadow-xl shadow-inner border-4 border-indigo-200 border-r-indigo-500">
           <h2 className="font-bold">Personal Details</h2>
           <div className="w-full h-full flex items-center justify-center">
             <div>
@@ -58,7 +58,7 @@ const App = () => {
           </div>
         </div>
 
-        <div className="w-full aspect-video bg-slate-200 rounded-lg p-4 m-4 shadow-xl shadow-inner border-4 border-indigo-200 border-r-indigo-500">
+        <div className="w-full aspect-auto bg-slate-200 rounded-lg p-4 m-4 shadow-xl shadow-inner border-4 border-indigo-200 border-r-indigo-500">
           <h2 className="font-bold">Contact Details</h2>
           <div className="w-full h-full flex items-center justify-center">
             <div className="w-full p-3 text-xs rounded-lg bg-white bg-opacity-20 m-2">
@@ -78,7 +78,7 @@ const App = () => {
             </div>
           </div>
         </div>
-        <div className="w-full aspect-video bg-slate-200 rounded-lg p-4 m-4 shadow-xl shadow-inner border-4 border-indigo-200 border-r-indigo-500">
+        <div className="w-full aspect-auto bg-slate-200 rounded-lg p-4 m-4 shadow-xl shadow-inner border-4 border-indigo-200 border-r-indigo-500">
           <h2 className="font-bold">Other Details</h2>
           <div className="flex flex-row justify-between m-4">
             <div className="w-[calc(40%-0.1rem)] p-3 text-xs rounded-lg bg-white bg-opacity-20">
