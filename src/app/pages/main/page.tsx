ad07cff140478fc4c5dfa841db5afba911d7a121
Parthib DS <parthib22.ds@gmail.com>
2024-05-09 01:13:36 +0530

    Main page dynamic UI

--- src/app/pages/main/page.tsx
+++ src/app/pages/main/page.tsx
@@ -6,13 +6,22 @@ import { useEffect, useState } from "react";
 export default function Main() {
   const [width, setWidth] = useState(0);
   const [toggleMenu, setToggleMenu] = useState(false);
-  const [blurValue, setBlurValue] = useState(0);
+  const [scrollValue, setScrollValue] = useState(0);
+  const [greeting, setGreeting] = useState("Hello,");
+
+  useEffect(() => {
+    const date = new Date();
+    const hours = date.getHours();
+    if (hours >= 4 && hours < 12) setGreeting("Good Morning,");
+    else if (hours >= 12 && hours < 17) setGreeting("Good Afternoon,");
+    else if (hours >= 17 && hours < 23) setGreeting("Good Evening,");
+    else setGreeting("Goodnight,");
+  }, []);
 
   useEffect(() => {
     const handleScroll = () => {
       var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-      setBlurValue(Math.min(1, scrollTop / 50));
-      //   console.log(scrollTop);
+      setScrollValue(Math.min(1, scrollTop / 50));
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -29,139 +38,234 @@ export default function Main() {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  return (
-    <>
-      <section
-        className={`fixed z-30 ${
-          toggleMenu ? "top-0 shadow-lg" : " top-[-272px]"
-        } w-screen bg-white p-4`}
-      >
-        <button onClick={() => setToggleMenu(false)}>
-          <Image
-            src={
-              "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAACSklEQVR4nO2awU4TURSGv5WDCw0mCu5k6QJXuNSdgZhoZFngGYiEt0BcaDQxbn0EtVHegBjXQgsrkBKMuBYhHHOTQ9KUmWk7c+6dO4Q/+ZM2bc+cP/+d23POHbjExcUNYBZYBZrAJvAHOFK61xv6mfvOM2CUSJAAC8AacALIkHS/+QLMa6zguAosA3sFks/iT2AJGAkl4jGwbSigl1vAjE8BzvrXHgX08oM6b4px4HtAEaL8BoxZiZhQu6UitjWHUril26hUzG3gdlERIxUtJ8lZZoW26HcRJC89fFNki5VIOT2oCGdfK4KEJYNbg/5pLntK4ACYAh4YxHo+iBt7nkRM6jXuG8Tb7XfjL3gWManvLeLO5QlZMxbxC7inse8C+4axm3n9xEkNnBDlMXA9TchsTZyQLj5JE/KyJk5IF1+kCWnWyAlRfkwT0q6RE6J0Be05HNbICem67jkclQg4FdgJUf690EIODZdWp8ql1arhzb4RavvtVLH9rhoFD+nMSugSpROyRBmtWdH4L6todPhqfDGfznwmB/Me7PflTCNPSKJT8dhb3R3gCn2w5EHImRgn4qFBrMV+Is5ciWFMKhlsDzNxnIkgYUnhKfCIIfE2gsSlh68ogEQHxxIJ1we5wbNwM6JjhXFKYsKgDS7DFnAHI4xVtMzW9bDJy2HoaSAR732fvU97XmqbRbbYokh0tL9rKMCVHYtVPQGR6FS8qbPYYZM/1iq2UWZrtYbrDZ7qGPOT9tO/ux6qca9/aHu6ok3RNfMsLkEc+A+TWzlJWK35FAAAAABJRU5ErkJggg=="
-            }
-            height={45}
-            width={45}
-            alt="back"
-          />
-        </button>
-        <ul className="flex flex-col items-center gap-4">
-          <li>
-            <Link href={""}>Option1</Link>
-          </li>
-          <li>
-            <Link href={""}>Option2</Link>
-          </li>
-          <li>
-            <Link href={""}>Option3</Link>
-          </li>
-          <li>
-            <Link href={""}>Option4</Link>
-          </li>
-          <li>
-            <Link href={""}>Option5</Link>
-          </li>
-        </ul>
-      </section>
-      <nav
-        className="fixed z-20 flex items-center w-screen gap-2 p-4"
-        style={{
-          backgroundColor: `rgba(255 255 255 / ${blurValue})`,
-          boxShadow: `0 2px 4px 0 rgba(0 0 0 / ${blurValue / 10})`,
-        }}
-      >
-        <button onClick={() => setToggleMenu(true)}>
-          <Image
-            src={
-              "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAACNklEQVR4nO2azUocURCFv1XGLJQREs1OnyOaXVACEV368wwhg2+hZpGQQMg2j5A4RN9AxLVxRldqNkGz9g9LLlQgTLqnu3puT5eDBw409Mztc/pWV9+q2/CAwcUosABsAE3gAPgDXCrD8U89F34zD9RxghqwAmwDN4AYGf7zA1jWsfqOx8Aq8KuA+DSeAg1gqF8mXgFHEQ108hCYLdNAmPoPJRro5Fed+agYB/b6aEKUu8BYLBOTOt1SEduqoSc81TQqFfMIeFbUxFBF4SRdwqxQiv7sQLx08GORFCtOOZPXRJi+lgPBksLDvC/NVQdiJYNv88zGqQOhksGTrAd/xYFIycmlbka2HQiUnAzlQGo9YVmKPyc+pgzXvwZGkgZZMN6R6RKMvDBqmEsa5J2DcBEj15OMNB0IEyO/JRlpOxAmRoYF7X84dyBMjPydZOTSOEjVWUuAixhGPGSti4EOrZYDYWJkaPgNbvrdcCBMjFxLMjJ/D7PW66RB6sZFY7hobEwbrn+VtmgM2HIQLpKTm93uyLIDgZKTi4NS6j7KitOGA6GSwTdZJv7Oioc2qaSwbek4zjoQLAm8BV5ixCcHwqWD7ymAmjaOxQl38jzgaXjiaFthnB4xWXEZ3AImiISxisJsRzebStkMve2TiS9l773PlBxqB0VSbFHUtLV/EtHAsb6xK/kCoqZd8ab2Yq3ir3UVu9hLao2NEe3Fhjbmd62nz/75qCYc72t5uqZF0XB0FQ/AB+4Avh/z0Htx8AEAAAAASUVORK5CYII="
-            }
-            height={50}
-            width={50}
-            alt="menu"
-          />
-        </button>
-        <input
-          type="text"
-          placeholder="Search on Rxkeep"
-          className="h-[46px] px-3 font-light text-sm text-left border-[1.5px] border-black rounded-full bg-transparent"
-        />
-      </nav>
-      <main className="relative mainMain h-full w-screen flex flex-col gap-4 p-4">
-        {/* <nav className="fixed w-screen h-20 border-t-orange-600 border-t-8 flex items-center justify-around">
-        <h1 className="font-extrabold text-3xl text-blue-900">
-          R<sub>x</sub> <span className="text-2xl font-black">KEEP</span>
-        </h1>
-        <ul className="flex gap-8">
-          <li className="hover:text-orange-700">
-            <Link href={""}>Option1</Link>
-          </li>
-          <li className="hover:text-orange-700">
-            <Link href={""}>Option2</Link>
-          </li>
-          <li className="hover:text-orange-700">
-            <Link href={""}>Option3</Link>
-          </li>
-          <li className="hover:text-orange-700">
-            <Link href={""}>Option4</Link>
-          </li>
-          <li className="hover:text-orange-700">
-            <Link href={""}>Option5</Link>
-          </li>
-        </ul>
-      </nav> */}
-
-        <section className="flex flex-col gap-4 mt-12">
-          <div className="flex flex-col m-10 mx-2">
-            <span className="mb-4 text-4xl">Good Morning,</span>
-            <span className="font-medium text-5xl">Parthib</span>
-          </div>
+  if (width <= 840)
+    return (
+      <>
+        <section
+          className={`fixed z-30 ${
+            toggleMenu ? "top-0 border-b-[1px] border-b-black" : " top-[-300px]"
+          } w-screen bg-white p-4 pb-10`}
+        >
+          <button onClick={() => setToggleMenu(false)}>
+            <Image src={"/back.png"} height={50} width={50} alt="back" />
+          </button>
+          <ul className="flex flex-col items-center gap-4">
+            <Link href={""}>
+              R<sub>x</sub>Meds
+            </Link>
+            <Link href={""}>Profile</Link>
+            <Link href={""}>Settings</Link>
+          </ul>
         </section>
-        <section className="flex w-full aspect-square gap-4 text-white">
-          <div className="relative flex-1 bg-red-300 rounded-xl">
-            <div className="absolute top-4 left-4 z-10">
-              <span className="text-sm drop-shadow-sm">Prescriptions</span>
-              <h1 className="text-6xl font-bold drop-shadow-sm">102</h1>
+        <nav
+          className="fixed w-screen z-20 flex justify-between items-center gap-2 p-4"
+          style={{
+            // backdropFilter: `blur(${scrollValue * 10}px)`,
+            backgroundColor: `rgba(255 255 255 / ${scrollValue})`,
+            borderBottom: `1px solid rgba(0 0 0 / ${scrollValue / 2})`,
+          }}
+        >
+          <button
+            className="w-[50px] h-[50px]"
+            onClick={() => setToggleMenu(true)}
+          >
+            <Image src={"/menu.png"} height={50} width={50} alt="menu" />
+          </button>
+          <input
+            type="text"
+            placeholder="Search anything..."
+            className="w-40 h-[46px] px-4 font-light text-sm text-left border-[1.5px] border-black rounded-full bg-transparent placeholder:italic"
+          />
+        </nav>
+        <Link
+          href={""}
+          className="scanBtn fixed z-30 bottom-4 right-4 flex border-black bg-transparent rounded-full aspect-square"
+        >
+          <Image src={"/add.png"} height={70} width={70} alt="add" />
+        </Link>
+        <main className="relative mainMain h-full w-screen flex flex-col gap-4 p-4 pb-24">
+          <section className="flex flex-col gap-4 mt-12">
+            <div className="flex flex-col m-10 mx-2">
+              <span className="mb-4 text-4xl">{greeting}</span>
+              <span className="font-bold text-5xl">Parthib</span>
             </div>
-            <Image
-              src={"/prescriptions.svg"}
-              height={500}
-              width={500}
-              alt="prescriptions"
-              className="absolute z-0 bottom-[-18.5px]"
-            />
-          </div>
-          <div className="flex-1 flex flex-col gap-4">
-            <div className="relative flex-1 bg-green-300 rounded-xl">
+          </section>
+          <section className="flex w-full aspect-square gap-2 text-black text-opacity-90">
+            <Link
+              href={""}
+              className="relative flex-1 bg-red-300 rounded-xl overflow-hidden"
+            >
               <div className="absolute top-4 left-4 z-10">
-                <span className="text-sm drop-shadow-sm">Doctors</span>
-                <h1 className="text-6xl font-bold drop-shadow-sm">6</h1>
+                <span className="text-sm drop-shadow-sm">Prescriptions</span>
+                <h1 className="text-6xl font-bold drop-shadow-sm">102</h1>
               </div>
               <Image
-                src={"/doctors.svg"}
-                height={500}
-                width={750}
-                alt="doctors"
-                className="absolute z-0 bottom-[-10px]"
-              />
-            </div>
-            <div className="relative flex-1 bg-orange-300 rounded-xl">
-              <div className="absolute top-4 left-4 z-10">
-                <span className="text-sm drop-shadow-sm">Medicines</span>
-                <h1 className="text-6xl font-bold drop-shadow-sm">69</h1>
-              </div>
-              <Image
-                src={"/medicines.svg"}
+                src={"/prescriptions.svg"}
                 height={500}
                 width={500}
-                alt="medicines"
-                className="scale-75 absolute z-0 bottom-[-20px] right-[-20px]"
+                alt="prescriptions"
+                className="absolute z-0 bottom-0"
               />
+            </Link>
+            <div className="flex-1 flex flex-col gap-2">
+              <Link
+                href={""}
+                className="relative flex-1 bg-green-300 rounded-xl overflow-hidden"
+              >
+                <div className="absolute top-4 left-4 z-10">
+                  <span className="text-sm drop-shadow-sm">Doctors</span>
+                  <h1 className="text-6xl font-bold drop-shadow-sm">7</h1>
+                </div>
+                <Image
+                  src={"/doctors.svg"}
+                  height={500}
+                  width={500}
+                  alt="doctors"
+                  className="scale-75 absolute z-0 bottom-[-10%] right-[-20%]"
+                />
+              </Link>
+              <Link
+                href={""}
+                className="relative flex-1 bg-orange-300 rounded-xl overflow-hidden"
+              >
+                <div className="absolute top-4 left-4 z-10">
+                  <span className="text-sm drop-shadow-sm">Medicines</span>
+                  <h1 className="text-6xl font-bold drop-shadow-sm">69</h1>
+                </div>
+                <Image
+                  src={"/medicines.svg"}
+                  height={500}
+                  width={500}
+                  alt="medicines"
+                  className="scale-75 absolute z-0 bottom-[-10%] right-[-10%]"
+                />
+              </Link>
             </div>
-          </div>
-        </section>
-        <section className="w-full aspect-square bg-blue-300 rounded-xl"></section>
-      </main>
-    </>
-  );
+          </section>
+          <section className="w-full aspect-square bg-blue-300 rounded-xl grid place-items-center">
+            more...
+          </section>
+        </main>
+      </>
+    );
+  else
+    return (
+      <>
+        <nav
+          className="fixed z-30 w-screen h-20 px-28 flex items-center justify-between"
+          style={{
+            // backdropFilter: `blur(${scrollValue * 10}px)`,
+            backgroundColor: `rgba(255 255 255 / ${scrollValue})`,
+            borderBottom: `1px solid rgba(0 0 0 / ${scrollValue / 2})`,
+          }}
+        >
+          <Link
+            href={"/main"}
+            className="font-extrabold text-3xl text-blue-900"
+          >
+            R<sub>x</sub>
+            {width >= 950 && " "}
+            <span className="text-2xl font-black">
+              K{width >= 950 && `EEP`}
+            </span>
+          </Link>
+          <ul className="flex gap-2">
+            {/* <Link href={""} className="hover:text-orange-700">
+              Upload
+            </Link>
+            <span className="text-slate-300">|</span> */}
+            <Link
+              href={""}
+              title="Compare medicines and buy them at lowest cost"
+              className="hover:text-orange-700 font-semibold"
+            >
+              R<sub>x</sub>Meds
+            </Link>
+            <span className="text-slate-300">|</span>
+            <Link
+              href={""}
+              title="Account & settings"
+              className="hover:text-orange-700"
+            >
+              Settings
+            </Link>
+          </ul>
+          <input
+            type="text"
+            placeholder="Search anything..."
+            className="w-40 h-[46px] px-4 font-light text-sm text-left border-[1.5px] border-black rounded-full bg-transparent placeholder:italic"
+          />
+        </nav>
+        <main className="relative mainMain min-h-screen h-full w-screen flex flex-row gap-4 py-10 px-28">
+          <section className="w-[calc(50%-0.5rem)] flex-1 flex flex-col gap-4 mt-12 text-black text-opacity-90">
+            <div className="flex flex-col gap-4 ">
+              <div className="relative flex flex-col m-10 mx-2">
+                <span className="mb-4 text-4xl">{greeting}</span>
+                <span className="font-bold text-5xl">Parthib</span>
+                <Link
+                  href={""}
+                  title="Upload Prescription"
+                  className="h-[50px] scanBtn absolute z-30 bottom-0 right-5 flex border-black bg-transparent rounded-full aspect-square"
+                >
+                  <Image src={"/add.png"} height={50} width={50} alt="add" />
+                </Link>
+              </div>
+            </div>
+            <Link
+              href={""}
+              className="relative flex-1 bg-red-300 rounded-2xl overflow-hidden"
+            >
+              <div className="absolute top-4 left-4 z-10">
+                <span className="drop-shadow-sm">Prescriptions</span>
+                <h1 className="text-8xl font-bold drop-shadow-sm">102</h1>
+              </div>
+              <Image
+                src={"/prescriptions.svg"}
+                height={500}
+                width={500}
+                alt="prescriptions"
+                className="scale-125 h-full absolute z-0 bottom-0 right-[-20%]"
+              />
+            </Link>
+            <div className="flex-1 flex flex-row gap-4">
+              <Link
+                href={""}
+                className="relative flex-1 bg-green-300 rounded-2xl overflow-hidden"
+              >
+                <div className="absolute top-4 left-4 z-10">
+                  <span className="drop-shadow-sm">Doctors</span>
+                  <h1 className="text-8xl font-bold drop-shadow-sm">7</h1>
+                </div>
+                <Image
+                  src={"/doctors.svg"}
+                  height={500}
+                  width={500}
+                  alt="doctors"
+                  className="h-full absolute z-0 bottom-0 right-[-20%]"
+                />
+              </Link>
+              <Link
+                href={""}
+                className="relative flex-1 bg-orange-300 rounded-2xl overflow-hidden"
+              >
+                <div className="absolute top-4 left-4 z-10">
+                  <span className="drop-shadow-sm">Medicines</span>
+                  <h1 className="text-8xl font-bold drop-shadow-sm">69</h1>
+                </div>
+                <Image
+                  src={"/medicines.svg"}
+                  height={500}
+                  width={500}
+                  alt="medicines"
+                  className="h-full absolute z-0 bottom-[-10%] right-[-20%]"
+                />
+              </Link>
+            </div>
+          </section>
+          <section className="w-[calc(50%-0.5rem)] mt-12 flex-1 bg-blue-300 rounded-2xl grid place-items-center">
+            more...
+          </section>
+        </main>
+      </>
+    );
 }
